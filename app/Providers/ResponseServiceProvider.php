e2c3318ab69807ac68f488dfe4d29fe719591687
adio ihsan <adioihsan@gmail.com>
2024-02-01 09:42:23 +0700

    feat: add pagination support on json response

--- app/Providers/ResponseServiceProvider.php
+++ app/Providers/ResponseServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Http\Resources\Json\JsonResource;
+// use Illuminate\Http\JsonResponse;
 
 class ResponseServiceProvider extends ServiceProvider
 {
@@ -21,21 +23,24 @@ class ResponseServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // define default api response template
-        Response::macro("api_ok",function($message="",$data=[],$status_code=200){
-            $response = [
+        Response::macro("api_ok",function($message="",$resource,$status_code=200){
+            $result = [
                 "success" => true,
                 "message" => $message,
-                "data"=>$data,
             ];
-            return response()->json($response,$status_code);
+            if(count($resource) >1){
+                $result = array_merge($result,$resource);
+            }
+            return $result;
         });
+
         Response::macro("api_fail",function($message="",$data=[],$status_code=400){
-            $response = [
+            $result = [
                 "success" => false,
                 "message" => $message,
                 "data"=>$data,
             ];
-            return response()->json($response,$status_code);
+            return response()->json($result,$status_code);
         });
     }
 }
