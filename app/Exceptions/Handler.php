ffc829aa594b4022a86c8a6b0483032090bc9453
adio ihsan <adioihsan@gmail.com>
2024-02-01 12:28:58 +0700

    feat : add exception hanlder

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,10 +3,20 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+
+    /**
+     * Indicates that an exception instance should only be reported once.
+     *
+     * @var bool
+     */
+    protected $withoutDuplicates = true;
+
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -23,6 +33,15 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
+        $this->renderable(function (\Throwable $exception, $request) {
+            if($exception instanceof NotFoundHttpException){
+                $statusCode = $exception->getStatusCode() ?: 404;
+                $message = $exception->getMessage();
+                return response()->api_fail($message,[],$statusCode);
+            }
+            return response()->api_fail();
+        });
+        
         $this->reportable(function (Throwable $e) {
             //
         });
