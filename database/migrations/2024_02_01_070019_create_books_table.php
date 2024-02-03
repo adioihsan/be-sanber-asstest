8b69feae5aaf56fd98dbd5a8adcbc947ab1e96a3
adio ihsan <adioihsan@gmail.com>
2024-02-03 21:25:12 +0700

    fix: change book description type to text

--- database/migrations/2024_02_01_070019_create_books_table.php
+++ database/migrations/2024_02_01_070019_create_books_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string("title");
-            $table->string("description");
+            $table->text("description");
             $table->string("image_url");
             $table->integer("release_year");
             $table->string("price");
